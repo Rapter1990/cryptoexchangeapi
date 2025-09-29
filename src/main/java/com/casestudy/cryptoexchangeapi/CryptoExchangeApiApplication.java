f3e6a2df77c133c2c98b0ef5b89891e84e485dee
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-09-29 15:01:05 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/casestudy/cryptoexchangeapi/CryptoExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.cryptoexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoExchangeApiApplication.class, args);
+	}
+
+}
